116263885102f4dcb00156881190c287d8d6cfc2
Lúcás Críostóir Meier <cronokirby@gmail.com>
2022-03-19 14:45:35 +0100

    Setup tailwindcss3 with empty style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: {
-    content: ["./layouts/**/*.html"],
-  },
+  content: ["./layouts/**/*.html"],
   theme: {
-    fontFamily: {
-      mono: ["IBM Plex Mono", "JetBrains Mono", "monospace"],
-      serif: ["IBM Plex Serif", "Noto Serif", "Georgia", "Cambria", "Times New Roman", "serif"],
-    },
     extend: {},
   },
-  variants: {},
   plugins: [],
-};
+}
